3e01dd5d5f3563e8ac4609b4b2c316ccb74ac382
Krzysztof <krzysztofs012@outlook.com>
2020-11-20 22:22:58 +0100

    Added new routes: /quizzes, /users:id

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,6 +5,9 @@ const messages = require('../utils/responseMessages');
 const { validatePhoto } = require('../utils/functions');
 const { csrfProtection } = require('../middlewares/csrf');
 const { addUserController } = require('../controllers/apiController');
+const verify = require('../middlewares/verifyToken');
+const Quiz = require('../models/Quiz');
+const User = require('../models/User');
 
 router.post(
   '/adduser',
@@ -24,6 +27,24 @@ router.post(
 
 router.get('/getcsrf', csrfProtection, (req, res) => {
   res.send({ csrfToken: req.csrfToken() });
+});
+
+router.post('/quizzes', verify, async (req, res) => {
+  if (req.user) {
+    const quizzes = await Quiz.find({});
+    res.send(quizzes);
+  }
+});
+
+router.post('/users/:id', verify, async (req, res) => {
+  if (req.user) {
+    try {
+      const [searchedUser] = await User.find({ _id: req.params.id });
+      return res.send(searchedUser);
+    } catch (err) {
+      res.status(400).send(`Couldn't find user`);
+    }
+  }
 });
 
 module.exports = router;
