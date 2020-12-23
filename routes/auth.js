518844d39b9517a87560a4106825c207dd6ead14
Krzysztof <krzysztofs012@outlook.com>
2020-12-23 20:42:06 +0100

    Removed all logic and put in the authService

---
+++ routes/auth.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const { check } = require('express-validator');
+const messages = require('../utils/responseMessages');
+
+const { csrfProtection } = require('../middlewares/csrf');
+const { validatePhoto } = require('../utils/functions');
+const { authController } = require('../controllers/auth');
+
+const router = express.Router();
+
+router.post(
+  '/login',
+  [
+    check('nickname', messages.INVALID_NICKNAME).trim().isLength({ min: 3, max: 15 }),
+    check('password', messages.INVALID_PASSWORD).trim().isLength({ min: 8, max: 15 }),
+  ],
+  authController.login,
+);
+
+router.post(
+  '/register',
+  [
+    check('nickname', messages.INVALID_NICKNAME).trim().isLength({ min: 3, max: 15 }),
+    check('password', messages.INVALID_PASSWORD).trim().isLength({ min: 8, max: 15 }),
+    check('confirmedPassword', messages.DIFFERENT_PASSWORDS)
+      .exists()
+      .trim()
+      .custom((confirmedPassword, { req }) => confirmedPassword === req.body.password),
+    check('photo', messages.PHOTO_REQUIRED)
+      .notEmpty()
+      .custom(photo => validatePhoto(photo)),
+  ],
+  authController.register,
+);
+
+router.get('/csrf', csrfProtection, authController.getCsrf);
+
+module.exports = router;
