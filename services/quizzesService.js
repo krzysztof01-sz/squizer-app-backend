edc5cd28e3752f3a937f634a5f6e6a77aa324226
Krzysztof <krzysztofs012@outlook.com>
2021-02-04 22:25:04 +0100

    Added new methods: getQuiz and getComments

--- services/quizzesService.js
+++ services/quizzesService.js
@@ -1,13 +1,14 @@
 const Quiz = require('../models/Quiz');
+const { QuizComment } = require('../models/QuizComment');
 const QuizQuestion = require('../models/QuizQuestion');
-const { makeResponse, normalizeResponse } = require('../utils/functions');
+const { makeResponse, getArrayOf, validate } = require('../utils/functions');
 const messages = require('../utils/responseMessages');
 const responseTypes = require('../utils/responseTypes');
 
 class QuizzesService {
   async getAllQuizzes() {
     try {
-      const quizzes = await Quiz.find({});
+      const quizzes = await Quiz.find({}).sort({ creationDate: -1 });
       if (quizzes.length > 0) {
         return {
           type: responseTypes.success,
@@ -25,10 +26,37 @@ class QuizzesService {
       const { questions } = response;
       if (questions.length > 0) {
         return {
+          questions,
           type: responseTypes.success,
-          data: questions,
         };
       } else throw messages.QUIZ_DOESNT_EXISTS;
+    } catch (e) {
+      if (typeof e === 'object') e = messages.QUIZ_DOESNT_EXISTS;
+      return makeResponse(e, responseTypes.error);
+    }
+  }
+
+  async getComments(quizId) {
+    try {
+      const comments = await QuizComment.find({ quizId }).sort({ creationDate: -1 });
+      return {
+        comments,
+        type: responseTypes.success,
+      };
+    } catch (e) {
+      if (typeof e === 'object') e = messages.COMMENTS_NOT_FOUND;
+      return makeResponse(e, responseTypes.error);
+    }
+  }
+
+  async getQuiz(quizId) {
+    try {
+      const [quiz] = await Quiz.find({ _id: quizId });
+      if (!quiz) throw messages.QUIZ_DOESNT_EXISTS;
+      return {
+        type: responseTypes.success,
+        data: quiz,
+      };
     } catch (e) {
       if (typeof e === 'object') e = messages.QUIZ_DOESNT_EXISTS;
       return makeResponse(e, responseTypes.error);
@@ -41,20 +69,23 @@ class QuizzesService {
     const quiz = { title, category, description, createdBy: req.user._id };
 
     try {
+      const validationResult = validate(req);
+      if (!validationResult.isEmpty()) throw validationResult.array();
+
       const createdQuiz = await Quiz.create({ ...quiz });
       const createdQuestions = await QuizQuestion.create({ quizId: createdQuiz.id, questions });
 
       if (createdQuiz._id && createdQuestions._id) {
         return {
           type: responseTypes.success,
-          msg: normalizeResponse(makeResponse(`Quiz: ${createdQuiz.title} has created.`, 'success')),
+          msg: getArrayOf(makeResponse(`Quiz: ${createdQuiz.title} has created.`, 'success')),
         };
       } else throw messages.INVALID_QUIZ_DATA;
     } catch (e) {
       if (typeof e === 'object') e = messages.INVALID_QUIZ_DATA;
       return {
         type: responseTypes.error,
-        msg: normalizeResponse(makeResponse(e, responseTypes.error)),
+        msg: getArrayOf(makeResponse(e, responseTypes.error)),
       };
     }
   }
