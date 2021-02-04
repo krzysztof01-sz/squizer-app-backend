4693874993ec54751360bf1cc40f044c744a8c8e
Krzysztof <krzysztofs012@outlook.com>
2021-02-04 22:15:27 +0100

    Added new controller: getQuiz

--- controllers/quizzes.js
+++ controllers/quizzes.js
@@ -21,11 +21,38 @@ module.exports.quizzesController = {
       const response = await QuizzesService.getQuestions(req.params.id);
       const { type } = response;
       if (type === responseTypes.success) {
-        const { data } = response;
-        res.status(200).json({ type, questions: data });
+        const { questions } = response;
+        res.status(200).json({ type, questions });
       } else {
         const { msg } = response;
         res.status(400).json({ type, msg });
+      }
+    }
+  },
+
+  getComments: async (req, res) => {
+    if (req.user) {
+      const response = await QuizzesService.getComments(req.params.id);
+
+      const { type } = response;
+      if (type === responseTypes.success) {
+        const { comments } = response;
+        res.status(200).json({ type, comments });
+      } else {
+        const { msg } = response;
+        res.status(400).json({ type, msg });
+      }
+    }
+  },
+
+  getQuiz: async (req, res) => {
+    if (req.user) {
+      const id = req.params.id;
+      const result = await QuizzesService.getQuiz(id);
+      if (result.type === responseTypes.success) {
+        res.status(201).json(result);
+      } else {
+        res.status(400).json(result);
       }
     }
   },
