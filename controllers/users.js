229215ddc4f88bfac3b0519e0646fcbeafbd26c3
Krzysztof <krzysztofs012@outlook.com>
2021-02-09 22:29:59 +0100

    Added new controllers: getUsers and updateUserAfterGame

--- controllers/users.js
+++ controllers/users.js
@@ -14,4 +14,32 @@ module.exports.usersController = {
       }
     }
   },
+
+  getUsers: async (req, res) => {
+    if (req.user) {
+      const response = await UserService.getUsers();
+      const { type } = response;
+
+      if (type === responseTypes.success) {
+        const { users } = response;
+        res.status(200).json({ type, users });
+      } else {
+        res.status(400).json({ type });
+      }
+    }
+  },
+
+  updateUserAfterGame: async (req, res) => {
+    if (req.user) {
+      const userId = req.user._id;
+      const { quizId, points } = req.body;
+      const { type } = await UserService.updateUserAfterGame(userId, quizId, points);
+
+      if (type === responseTypes.success) {
+        res.status(200).json({ type });
+      } else {
+        res.status(400).json({ type });
+      }
+    }
+  },
 };
