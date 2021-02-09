18f10cf75d29be7213a61566833e5f3e5f52af2c
Krzysztof <krzysztofs012@outlook.com>
2021-02-09 21:59:57 +0100

    Added new methods: getUsers and updateUserAfterGame

--- services/userService.js
+++ services/userService.js
@@ -15,6 +15,45 @@ class UserService {
       return e;
     }
   }
+
+  async getUsers() {
+    try {
+      const response = await User.find({}).sort({ points: -1 });
+
+      const users = response.map(({ _id, nickname, points, avatarType }) => {
+        return {
+          _id,
+          nickname,
+          points,
+          avatarType,
+        };
+      });
+      if (users) {
+        return {
+          type: responseTypes.success,
+          users,
+        };
+      } else throw { type: responseTypes.error };
+    } catch (e) {
+      return e;
+    }
+  }
+
+  async updateUserAfterGame(userId, quizId, points) {
+    try {
+      const [userToUpdate] = await User.find({ _id: userId });
+
+      if (!userToUpdate._id) throw { type: responseTypes.error };
+
+      if (!userToUpdate.visitedQuizzes.includes(quizId)) {
+        await User.updateOne({ _id: userId }, { $inc: { points }, $push: { visitedQuizzes: quizId } });
+      }
+
+      return { type: responseTypes.success };
+    } catch (e) {
+      return e;
+    }
+  }
 }
 
 const Service = new UserService();
