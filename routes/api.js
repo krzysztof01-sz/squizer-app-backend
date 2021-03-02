6c4d818cae0c6499e42703a658e8f75e31c566e8
Krzysztof <krzysztofs012@outlook.com>
2021-03-02 23:43:09 +0100

    Added new user routes: /:id, /set-avatar, /correct-answers-rate and /:id/quizzes

--- routes/api.js
+++ routes/api.js
@@ -7,9 +7,13 @@ const { commentsController } = require('../controllers/comments');
 const { check } = require('express-validator');
 const { maxDescriptionLength } = require('../utils/constants');
 
-router.get('/users/:id', verify, usersController.getUser);
 router.get('/users', verify, usersController.getUsers);
 router.put('/users', verify, usersController.updateUserAfterGame);
+router.get('/users/:id', verify, usersController.getUser);
+router.get('/users/:id/quizzes', verify, usersController.getUserQuizzes);
+router.get('/users/:id/correct-answers-rate', verify, usersController.getUserCorrectAnswersRate);
+router.put('/users/:id/set-avatar/:avatarType', verify, usersController.setUserAvatarType);
+router.get('/account', verify, usersController.getProfileData);
 router.get('/quizzes', verify, quizzesController.getAll);
 router.get('/quizzes/:id', verify, quizzesController.getQuiz);
 router.get('/quizzes/:id/questions', verify, quizzesController.getQuestions);
