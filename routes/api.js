c81891a77f8b7c7c32a5cac5f6983fc9c21188f6
Krzysztof <krzysztofs012@outlook.com>
2021-03-05 15:29:40 +0100

    Modified addComment route + ordered api routes

--- routes/api.js
+++ routes/api.js
@@ -1,23 +1,32 @@
 const express = require('express');
-const router = express.Router();
+const { check } = require('express-validator');
 const verify = require('../middlewares/verifyToken');
 const { quizzesController } = require('../controllers/quizzes');
 const { usersController } = require('../controllers/users');
-const { commentsController } = require('../controllers/comments');
-const { check } = require('express-validator');
 const { maxDescriptionLength } = require('../utils/constants');
+
+const router = express.Router();
 
 router.get('/users', verify, usersController.getUsers);
 router.put('/users', verify, usersController.updateUserAfterGame);
+
 router.get('/users/:id', verify, usersController.getUser);
 router.get('/users/:id/quizzes', verify, usersController.getUserQuizzes);
-router.get('/users/:id/correct-answers-rate', verify, usersController.getUserCorrectAnswersRate);
 router.put('/users/:id/set-avatar/:avatarType', verify, usersController.setUserAvatarType);
-router.get('/account', verify, usersController.getProfileData);
+
 router.get('/quizzes', verify, quizzesController.getAll);
 router.get('/quizzes/:id', verify, quizzesController.getQuiz);
 router.get('/quizzes/:id/questions', verify, quizzesController.getQuestions);
 router.get('/quizzes/:id/comments', verify, quizzesController.getComments);
+
+router.get('/account', verify, usersController.getProfileData);
+
+router.put(
+  '/quizzes/:id/comments',
+  [verify, check('content').trim().isLength({ min: 1, max: 500 })],
+  quizzesController.addComment,
+);
+
 router.post(
   '/quizzes',
   [
@@ -26,12 +35,6 @@ router.post(
     check('description').trim().isLength({ min: 3, max: maxDescriptionLength }),
   ],
   quizzesController.addQuiz,
-);
-
-router.post(
-  '/comments',
-  [verify, check('content').trim().isLength({ min: 1, max: 500 })],
-  commentsController.addComment,
 );
 
 module.exports = router;
