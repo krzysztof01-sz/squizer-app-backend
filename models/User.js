035ff50fd1935f1ea4e219293052af0c4bc462e1
Krzysztof <krzysztofs012@outlook.com>
2021-03-12 17:23:04 +0100

    Modified user schema

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { photoTypes } = require('../utils/photoTypes');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema(
@@ -16,7 +17,7 @@ const userSchema = new Schema(
     avatarType: {
       type: String,
       required: true,
-      enum: ['custom', 'default'],
+      enum: [photoTypes.custom, photoTypes.default],
     },
     stats: {
       correctAnswers: {
