25e150a275d968bd9bb88624530730975835bea5
Krzysztof <krzysztofs012@outlook.com>
2021-05-20 14:06:00 +0200

    Modified API routes

--- routes/api.js
+++ routes/api.js
@@ -1,39 +1,38 @@
-const express = require('express');
-const { check } = require('express-validator');
-const verify = require('../middlewares/verifyToken');
-const { quizzesController } = require('../controllers/quizzes');
-const { usersController } = require('../controllers/users');
-const { maxDescriptionLength } = require('../utils/constants');
+const express = require("express");
+const { check } = require("express-validator");
+const verify = require("../middlewares/verifyToken");
+const { quizzesController } = require("../controllers/quizzes");
+const { usersController } = require("../controllers/users");
+const { maxDescriptionLength } = require("../utils/constants");
 
 const router = express.Router();
 
-router.get('/users', verify, usersController.getUsers);
-router.put('/users', verify, usersController.updateUserAfterGame);
+router.get("/users", verify, usersController.getUsers);
+router.put("/users", verify, usersController.updateUserStatistics);
 
-router.get('/users/:id', verify, usersController.getUser);
-router.get('/users/:id/quizzes', verify, usersController.getUserQuizzes);
-router.put('/users/:id/set-avatar/:avatarType', verify, usersController.setUserAvatarType);
+router.get("/users/:id", verify, usersController.getUser);
+router.get("/users/:id/quizzes", verify, usersController.getUserQuizzes);
+router.put("/users/:id/update-avatar/:type", verify, usersController.updateUserAvatarType);
+router.get("/me", verify, usersController.getProfileData);
 
-router.get('/quizzes', verify, quizzesController.getAll);
-router.get('/quizzes/:id', verify, quizzesController.getQuiz);
-router.get('/quizzes/:id/questions', verify, quizzesController.getQuestions);
-router.get('/quizzes/:id/comments', verify, quizzesController.getComments);
-router.delete('/quizzes/:id', verify, quizzesController.deleteQuiz);
+router.get("/quizzes", verify, quizzesController.getAll);
+router.get("/quizzes/:id", verify, quizzesController.getQuiz);
+router.get("/quizzes/:id/questions", verify, quizzesController.getQuestions);
+router.get("/quizzes/:id/comments", verify, quizzesController.getComments);
+router.delete("/quizzes/:id", verify, quizzesController.deleteQuiz);
 
-router.get('/account', verify, usersController.getProfileData);
-
-router.put(
-  '/quizzes/:id/comments',
-  [verify, check('content').trim().isLength({ min: 1, max: 500 })],
+router.post(
+  "/quizzes/:id/comments",
+  [verify, check("content").trim().isLength({ min: 1, max: 500 })],
   quizzesController.addComment,
 );
 
 router.post(
-  '/quizzes',
+  "/quizzes",
   [
     verify,
-    check('title').trim().isLength({ min: 3, max: 100 }),
-    check('description').trim().isLength({ min: 3, max: maxDescriptionLength }),
+    check("title").trim().isLength({ min: 3, max: 100 }),
+    check("description").trim().isLength({ min: 3, max: maxDescriptionLength }),
   ],
   quizzesController.addQuiz,
 );
