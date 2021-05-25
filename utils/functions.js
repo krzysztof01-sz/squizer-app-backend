b1ca136884620ba6387bc7ba196e2ce22b5f68d5
Krzysztof <krzysztofs012@outlook.com>
2021-05-25 23:42:20 +0200

    Added shuffleArray function

--- utils/functions.js
+++ utils/functions.js
@@ -1,30 +1,35 @@
-const { validationResult } = require('express-validator');
-const { photoTypes } = require('./photoTypes');
-const responseTypes = require('./responseTypes');
+const { validationResult } = require("express-validator");
+const { photoTypes } = require("./photoTypes");
+const responseTypes = require("./responseTypes");
 
-const prefferedExtensions = ['png', 'jpg', 'jpeg'];
+const prefferedExtensions = ["png", "jpg", "jpeg"];
 
-const validateAvatar = avatar => {
+const validateAvatar = (avatar) => {
   if (!avatar) return false;
   if (avatar === photoTypes.default) return true;
   else {
     if (avatar.name) {
-      return prefferedExtensions.includes(avatar.name.split('.')[1]);
+      return prefferedExtensions.includes(avatar.name.split(".")[1]);
     } else return false;
   }
 };
 
-const validate = req => {
+const validate = (req) => {
   return validationResult(req).formatWith(({ msg }) => {
     return { msg, type: responseTypes.error };
   });
 };
 
-const getArrayOf = resp => [resp].flat();
+const getArrayOf = (resp) => [resp].flat();
 
 const makeResponse = (msg, type) => ({
   msg,
   type,
 });
 
-module.exports = { validateAvatar, validate, getArrayOf, makeResponse };
+const shuffleArray = (arr) => {
+  arr = arr.sort(() => Math.random() - 0.5);
+  return arr;
+};
+
+module.exports = { validateAvatar, validate, getArrayOf, makeResponse, shuffleArray };
