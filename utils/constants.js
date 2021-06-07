1f2f27e5422a70d649ccf5e378a30c0e4cf88f89
Krzysztof <krzysztofs012@outlook.com>
2021-06-07 17:53:56 +0200

    Changed maxDescriptionLength value

--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,4 @@
-const maxDescriptionLength = 400;
-const quizCategories = ['English', 'General knowledge', 'Geography', 'IT', 'Maths', 'Riddles', 'Science', 'Other'];
+const maxDescriptionLength = 500;
+const quizCategories = ["English", "General knowledge", "Geography", "IT", "Maths", "Riddles", "Science", "Other"];
 
 module.exports = { maxDescriptionLength, quizCategories };
